8da9a7d18fb2409d208917620f2a68fcd07eae29
Dmitriy Chervan <dmitriy.chervan@yandex.ru>
2017-03-22 19:30:17 +0300

    Initial

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
+
+import 'style!css!foundation-sites/dist/css/foundation.min.css';
+$(document).foundation();
+
+import 'style!css!sass!applicationStyles';
+
+ReactDOM.render(
+  <p>Blplt</p>
+  document.querySelector('#app')
+);
