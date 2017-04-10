51d92840ab40755edbd1d9b787380ebb5924b343
Dmitriy Chervan <dmitriy.chervan@yandex.ru>
2017-04-10 20:18:59 +0300

    Added addTodo and toggleTodo reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import df from 'deep-freeze-strict';
-import { searchTextReducer, showCompletedReducer } from 'reducers';
+import { searchTextReducer, showCompletedReducer, todosReducer } from 'reducers';
 
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
@@ -12,13 +12,44 @@ describe('Reducers', () => {
       const res = searchTextReducer(df(''), df(action));
       expect(res).toEqual(action.searchText);
     });
+  });
 
+  describe('showCompletedReducer', () => {
     it('should toggle showCompleted', () => {
       const action = {
         type: 'TOGGLE_SHOW_COMPLETED'
       };
       const res = showCompletedReducer(df(false), df(action));
       expect(res).toBe(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      const action = {
+        type: 'ADD_TODO',
+        text: 'test'
+      };
+      const res = todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      const todos = [{
+        id: '123',
+        text: 'text',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      const action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      const res = todosReducer(df(todos), df(action));
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(null);
     });
   });
 });
