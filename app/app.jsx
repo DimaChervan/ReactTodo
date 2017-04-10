db0571fe11e81fd8b4f4e4084cfb16b98555ab4b
Dmitriy Chervan <dmitriy.chervan@yandex.ru>
2017-04-10 20:40:25 +0300

    Added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import TodoApp from 'TodoApp';
+import * as actions from 'actions';
+import { configure } from 'configureStore';
+
+const store = configure();
+store.subscribe(() => {
+  console.log('new state', store.getState());
+});
+
+store.dispatch(actions.addTodo('test'));
+store.dispatch(actions.setSearchText('search'));
+store.dispatch(actions.toggleShowCompleted());
 
 import 'style!css!foundation-sites/dist/css/foundation.min.css';
 $(document).foundation();
